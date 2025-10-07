fd57b63e17eaab101e540684859cee03aa233d0d
tsdmCode <116952397+tsdmCode@users.noreply.github.com>
2025-10-07 12:31:18 +0200

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+const mainCategories = {
+  Beauty: [],
+};
+
+let data;
+const fetchData = async () => {
+  try {
+    const response = await fetch('https://dummyjson.com/products');
+    data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getFeaturedProducts = () => {
+  fetch(`https://dummyjson.com/products/${Math.floor(Math.random() * data.total)}`)
+    .then((response) => response.json())
+    .then((product) => {
+      console.log(product);
+      const app = document.getElementById('app');
+
+      app.innerHTML += `<figure class="product-card">
+          <img src="${product.thumbnail}" />
+          <figcaption>    
+              <h2>${product.title}</h2>
+              <p>${product.description}</p>
+              <p>Price: $${product.price}</p>
+              <button onclick="addToCart()" class="buyBtn"></button>
+          </figcaption>
+      </figure>`;
+    })
+    .catch((error) => console.error(error));
+};
+
+const initialize = async () => {
+  await fetchData();
+  getFeaturedProducts();
+  getFeaturedProducts();
+  getFeaturedProducts();
+};
+
+initialize();
